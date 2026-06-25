48748c3e05e8ab0b058bf4087d67e88fefc660bb
Franksview <snelders.f@gmail.com>
2026-06-25 14:15:52 +0200

    Fix vercel.ts config

--- vercel.ts
+++ vercel.ts
@@ -1,7 +1,5 @@
-import { routes, type VercelConfig } from '@vercel/config/v1';
+import { type VercelConfig } from '@vercel/config/v1';
 
 export const config: VercelConfig = {
-  buildCommand: undefined,
   outputDirectory: 'public',
-  framework: 'static',
 };
